6668fb03db89c909d0252424672921d4387e5d3b
RudraSen <rudra.sen@technovista.com.bd>
2024-05-12 17:48:26 +0600

    prefiquence problem solve

---
+++ 800/prefiquence.py
@@ -0,0 +1,17 @@
+# Too Min Too Max
+for _ in range(int(input())):
+    t = list(map(int, input().split()))
+    m = input()
+    n = input()
+
+    i = 0
+    j = 0
+
+    while(i < len(m) and j < len(n)):
+        if(m[i] == n[j]):
+            i = i + 1
+            j = j + 1
+        else:
+            j = j + 1
+
+    print(i)

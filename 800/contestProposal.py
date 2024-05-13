59d125def8bb834eeee48478d22daa93c16bfb9e
RudraSen <rudra.sen@technovista.com.bd>
2024-05-13 13:38:50 +0600

    remove unnecessary code

--- 800/contestProposal.py
+++ 800/contestProposal.py
@@ -4,9 +4,6 @@ for _ in range(int(input())):
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
 
-    # a = sorted([4, 5, 6, 7, 8, 9])
-    # b = sorted([1, 2, 3, 4, 5, 6])
-
     result = 0
 
     for i in range(len(a)):
